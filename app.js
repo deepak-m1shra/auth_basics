0733ef41cc7ef6998403299bf13a3fbee81ac261
Deepak Mishra <deepakmishra@Deepaks-MacBook-Air.local>
2022-08-31 18:17:08 +0530

    Using bcrypt to encrypt

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const encrypt = require('mongoose-encryption')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const app = express()
 
@@ -22,7 +24,6 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 console.log(process.env)
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] })
 
 const User = new mongoose.model('User', userSchema)
 
@@ -43,9 +44,14 @@ app.post('/login', (req, res) => {
             console.log(err)
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render('secrets')
-                }
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result) {
+                        res.render('secrets')
+                    } else {
+                        res.send('<h1 style="color:red">Invalid credentials</h1>')
+                    }
+
+                });
             }
         }
     })
@@ -56,18 +62,23 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log('Error occured : ' + err)
+            } else {
+                res.render('secrets')
+            }
+        })
     })
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log('Error occured : ' + err)
-        } else {
-            res.render('secrets')
-        }
-    })
 })
 
 app.listen(3001, () => console.log('server is running at port ' + 3001))
